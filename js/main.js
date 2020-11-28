cae2431132144dc29325488c34e2ab77a19daa33
brian-crypto <britomflor066@gmail.com>
2020-11-28 20:45:38 -0300

    correguir

--- js/main.js
+++ js/main.js
@@ -17,3 +17,33 @@ document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
 overlay.addEventListener('click', (evento) =>{
     evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
 });
+
+//-------------------------------FORMULARIO-----------------------------------------//
+
+var label = document.querySelectorAll('.input__label');
+var input = document.querySelectorAll('.input__field');
+var labelContenido = document.querySelectorAll('.input__label-content');
+
+// input.addEventListener('blur', (evento) =>{
+//     if(evento.target.value.trim() !== ''){
+//         label.no-after;
+//     }else{
+//         ''
+//     }
+// });
+
+
+//-------------------------------CAROUSEL DE CLIENTES-----------------------------------------//
+
+
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.cards_carousel'), {
+        slidesToShow: 1,
+        dots: '.cards_carousel__indicadores',
+        draggable: true,
+        arrows: {
+        prev: '.cards__anterior',
+        next: '.cards__siguiente'
+        }
+      });
+})
