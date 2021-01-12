97da7a0462f13fa59893502e1032b5757d667afd
Daiana Ruiz <65079369+DaianaARuiz@users.noreply.github.com>
2021-01-12 15:24:35 -0300

    12/01

--- js/main.js
+++ js/main.js
@@ -18,23 +18,7 @@ overlay.addEventListener('click', (evento) =>{
     evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
 });
 
-//-------------------------------FORMULARIO-----------------------------------------//
-
-var label = document.querySelectorAll('.input__label');
-var input = document.querySelectorAll('.input__field');
-var labelContenido = document.querySelectorAll('.input__label-content');
-
-// input.addEventListener('blur', (evento) =>{
-//     if(evento.target.value.trim() !== ''){
-//         label.no-after;
-//     }else{
-//         ''
-//     }
-// });
-
-
 //-------------------------------CAROUSEL DE CLIENTES-----------------------------------------//
-
 
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.clientes__carousel__lista'), {
